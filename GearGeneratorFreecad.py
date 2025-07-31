be5c771a37cc972df38028c1fba397105b824844
Jesse Cornman <ocanath@gmail.com>
2025-07-30 18:36:04 -0700

    generator and calculator - next step is sympy solver

---
+++ GearGeneratorFreecad.py
@@ -0,0 +1,311 @@
+import FreeCAD as App
+import Part
+import freecad.gears.commands
+import numpy as np
+import os   #optional, part of the automatic .step file exports that occurs at the end
+
+
+"""
+Notes:
+
+This is a macro for generating a split ring compound planetary gearbox in FreeCAD.
+It requires Freecad 0.21.2 (or higher?) and the extremely excellent freecad gear plugin: https://github.com/looooo/freecad.gears
+
+In order to run it:
+
+1. Launch FreeCAD
+2. If not installed gear plugin, do it from within FreeCAD AddOn manager, follow the prompts and restart
+3. Open a new document
+4. Go to macros, and change location to the root directory of this repo
+5. Select this file and run it from within FreeCAD. It should generate your gearbox.
+"""
+
+print("Starting gear generator macro")
+
+#split ring compound planetary gear parameters
+p1_teeth = 21
+r1_teeth = 54
+r2_teeth = 53
+sun_teeth = r1_teeth - 2*p1_teeth
+p2_teeth = r2_teeth - sun_teeth - p1_teeth	#should be always p1 -1?
+pitch_mm = 0.5
+pressure_angle = 20 #degrees
+planet_bore = 3.55  #mm
+s1_shift = 0.0508
+noclearance = False
+
+#mechanical design specific parameters
+r1_height = 2.5   #mm
+r2_height = 3.5
+p2_height = 3.5
+r2_bearing_inner_race_ID = 30   #inner race ID of the bearing grabbing the top ring. If there's at least half a millimeter of clearance to this ID from the pitch diameter of the ring, we'll adjust the thickness to match this ID
+r2_r1_offset = 0.1
+
+#generator-specific options 
+num_points = 20
+
+#Helper functions for interfacing freecad to numpy and general math
+def Hz(angle):
+    return np.array( [
+        [np.cos(angle), -np.sin(angle), 0, 0],
+        [np.sin(angle), np.cos(angle),  0, 0],
+        [0,             0,              1, 0],
+        [0,             0,              0, 1]
+    ])
+
+def NumpyMatrixToFreecadMatrix(np_m):
+    fc_mat = App.Matrix()
+    # Define a mapping from matrix indices to FreeCAD attribute names:
+    attr_names = [
+        ["A11", "A12", "A13", "A14"],
+        ["A21", "A22", "A23", "A24"],
+        ["A31", "A32", "A33", "A34"],
+        ["A41", "A42", "A43", "A44"]
+    ]
+
+    for i in range(4):
+        for j in range(4):
+            setattr(fc_mat, attr_names[i][j], np_m[i, j])
+    return fc_mat
+
+def DeleteAllGears():
+    #clear the gears currently in the document
+    doc = FreeCAD.activeDocument()
+    num_deleted = 0
+    if doc is not None:
+        for obj in list(doc.Objects):
+            num_deleted = num_deleted + 1
+            if obj.TypeId == "Part::FeaturePython":
+                doc.removeObject(obj.Name)
+        doc.recompute()
+        print("Deleted", num_deleted, "Objects")
+
+
+
+#compute the carrier circle diameter
+carrier_planets_to_sun_distance = (p1_teeth * pitch_mm / 2) + (sun_teeth * pitch_mm / 2)    #carrier radius
+
+
+#create the placement matrices for the first stage planets
+m_c1_1 = Hz(0)
+m_c1_1[0][3] = carrier_planets_to_sun_distance
+#create the next two placement matrixes by rotating the first placement matrix by the carrier spacing (assumed to be even, so 120 and 240)
+ca = 120
+m_c2_1 = Hz(ca*np.pi/180).dot(m_c1_1)    #pre-multiply by 120 degrees rotation in z to put the gear in the right position
+m_c2_1 = m_c2_1.dot(Hz(-ca*np.pi/180*r1_teeth/p1_teeth))   #post multiply by the amount the gear would have rotated while meshed when being brought over to the carrier location
+ca = 120*2
+m_c3_1 = Hz(ca*np.pi/180).dot(m_c1_1)    #placement matrix for second planet
+m_c3_1 = m_c3_1.dot(Hz(-ca*np.pi/180*r1_teeth/p1_teeth))
+
+
+DeleteAllGears()
+
+#Ring 1
+r1 = freecad.gears.commands.CreateInternalInvoluteGear.create()
+r1.numpoints = num_points
+r1.num_teeth = r1_teeth
+r1.module = pitch_mm     #mm 
+r1.pressure_angle = pressure_angle  #degrees
+r1.properties_from_tool = True    #"if helix_angle is given and properties_from_tool is enabled, gear parameters are internally recomputed for the rotated gear"
+r1.thickness = r1.module*5   #mm. material added past the *addendum*? six teeth's worth seems decent
+r1.height = r1_height   #mm
+r1.shift = s1_shift
+if(noclearance == True):
+	r1.clearance = 0
+	r1.head = 0
+
+#helper function to re-call .create with the correct parameters
+def CreateP1Planet():
+    #Planet 1, stage 1
+    p1 = freecad.gears.commands.CreateInvoluteGear.create()
+    p1.numpoints = num_points
+    p1.module = pitch_mm
+    p1.pressure_angle = pressure_angle
+    p1.num_teeth = p1_teeth
+    p1.height = r1.height
+    p1.properties_from_tool = True
+    p1.axle_hole = True
+    p1.axle_holesize = planet_bore    #mm
+    p1.shift = -s1_shift
+    if(noclearance == True):
+        p1.clearance = 0
+        p1.head = 0
+    return p1
+
+
+
+#Planet 1, stage 1
+p1_1 = CreateP1Planet()
+m = NumpyMatrixToFreecadMatrix(m_c1_1)
+p1_1.Placement = FreeCAD.Placement(m)
+
+#Planet 2, stage 1
+p2_1 = CreateP1Planet()
+m = NumpyMatrixToFreecadMatrix(m_c2_1)
+p2_1.Placement = FreeCAD.Placement(m)
+
+#Planet 3, stage 1
+p3_1 = CreateP1Planet()
+m = NumpyMatrixToFreecadMatrix(m_c3_1)
+p3_1.Placement = FreeCAD.Placement(m)
+
+
+
+
+
+
+
+#Ring 2 (stage 2)
+r2 = freecad.gears.commands.CreateInternalInvoluteGear.create()
+r2.num_teeth = r2_teeth
+r2.module = pitch_mm     #mm 
+r2.numpoints = num_points
+r2.pressure_angle = pressure_angle  #degrees
+r2.properties_from_tool = True    #"if helix_angle is given and properties_from_tool is enabled, gear parameters are internally recomputed for the rotated gear"
+r2.height = r2_height   #mm
+r2.shift = s1_shift
+r2.thickness = r2.module*5   #mm. material added past the *addendum*? six teeth's worth seems decent
+if(noclearance == True):
+	r2.clearance = 0
+	r2.head = 0
+
+#recompute before adjusting the thickness again
+App.ActiveDocument.recompute()
+Gui.SendMsgToActiveView("ViewFit")
+dist_to_r2_bearing = FreeCAD.Units.Quantity(r2_bearing_inner_race_ID, "mm") - r2.pitch_diameter
+if(dist_to_r2_bearing > 0.5):
+    r2.thickness = (dist_to_r2_bearing)/2   #mm. material added past the *addendum*? six teeth's worth seems decent
+    print("Adjusted r2 thickness to match bearing")
+else:
+    print("Aborting due to pitch diameter being larger than top bearing ID.")
+#locate ring 2
+r2loc = Hz(0)
+r2loc[2][3] = r1_height+r2_r1_offset
+r2.Placement = FreeCAD.Placement(NumpyMatrixToFreecadMatrix(r2loc))
+
+
+#helper function to re-call .create with the correct parameters
+def CreateP2Planet():
+    #Planet 1, stage 1
+    p2 = freecad.gears.commands.CreateInvoluteGear.create()
+    p2.module = pitch_mm
+    p2.pressure_angle = pressure_angle
+    p2.numpoints = num_points
+    p2.num_teeth = p2_teeth
+    p2.height = p2_height
+    p2.properties_from_tool = True
+    p2.axle_hole = True
+    p2.axle_holesize = planet_bore    #mm
+    p2.shift = -s1_shift
+    if(noclearance == True):
+        p2.clearance = 0
+        p2.head = 0
+    return p2
+
+#create stack2 base planet (zero rotation/clocked with ring1)
+p1_2 = CreateP2Planet()
+p2_base_loc = Hz(0)
+p2_base_loc[2][3] = p1_1.height
+m_c1_2 = m_c1_1.dot(p2_base_loc) 
+p1_2.Placement = FreeCAD.Placement(NumpyMatrixToFreecadMatrix(m_c1_2))
+
+#locate second planet by rotating the first planet by 120 about the world Z axis, and about itself by the amount the gear would travel while doing that rotation
+p2_2 = CreateP2Planet()
+m_c2_2 = Hz(120*np.pi/180).dot(m_c1_2)
+m_c2_2 = m_c2_2.dot(Hz(-120*np.pi/180*r2_teeth/p2_teeth))
+p2_2.Placement = FreeCAD.Placement(NumpyMatrixToFreecadMatrix(m_c2_2))
+
+#locate the third planet by doing the same operation to the second planet! 
+p3_2 = CreateP2Planet()
+m_c3_2 = Hz(120*np.pi/180).dot(m_c2_2)
+m_c3_2 = m_c3_2.dot(Hz(-120*np.pi/180*r2_teeth/p2_teeth))
+p3_2.Placement = FreeCAD.Placement(NumpyMatrixToFreecadMatrix(m_c3_2))
+
+#recompute!
+App.ActiveDocument.recompute()
+Gui.SendMsgToActiveView("ViewFit")
+
+
+
+#End gear generation. Begin automatic export of gears to .step files
+###################################################################################################################################################
+
+
+# Determine the directory of the current script
+try:
+    script_dir = os.path.dirname(os.path.realpath(__file__))+str("/STEP/")
+except NameError:
+    raise RuntimeError("The __file__ variable is not defined. Cannot determine the script's directory.")
+
+print("Starting exports")
+doc = FreeCAD.activeDocument()
+objs = [
+    doc.getObject(p3_2.Name),
+    doc.getObject(p3_1.Name)
+]
+step_name = "p3Stack.step"
+export_path = os.path.join(script_dir, step_name)
+print("Exporting as", export_path)
+Part.export(objs, export_path) #r"C:\Users\Ocanath Robotman\Desktop\PLANETSTEP\p3Stack.step"
+
+
+objs = [
+    doc.getObject(p2_2.Name),
+    doc.getObject(p2_1.Name)
+]
+step_name = "p2Stack.step"
+export_path = os.path.join(script_dir, step_name)
+print("Exporting as", export_path)
+Part.export(objs, export_path) #r"C:\Users\Ocanath Robotman\Desktop\PLANETSTEP\p3Stack.step"
+
+
+doc = FreeCAD.activeDocument()
+objs = [
+    doc.getObject(p1_2.Name),
+    doc.getObject(p1_1.Name)
+]
+step_name = "p1Stack.step"
+export_path = os.path.join(script_dir, step_name)
+print("Exporting as", export_path)
+Part.export(objs, export_path) #r"C:\Users\Ocanath Robotman\Desktop\PLANETSTEP\p3Stack.step"
+
+
+doc = FreeCAD.activeDocument()
+objs = [
+    doc.getObject(r1.Name)
+]
+step_name = "r1.step"
+export_path = os.path.join(script_dir, step_name)
+print("Exporting as", export_path)
+Part.export(objs, export_path) #r"C:\Users\Ocanath Robotman\Desktop\PLANETSTEP\p3Stack.step"
+
+
+doc = FreeCAD.activeDocument()
+objs = [
+    doc.getObject(r2.Name)
+]
+step_name = "r2.step"
+export_path = os.path.join(script_dir, step_name)
+print("Exporting as", export_path)
+Part.export(objs, export_path) #r"C:\Users\Ocanath Robotman\Desktop\PLANETSTEP\p3Stack.step"
+
+
+
+
+doc = FreeCAD.activeDocument()
+objs = [
+    doc.getObject(r1.Name),
+    doc.getObject(r2.Name),
+    doc.getObject(p1_1.Name),
+    doc.getObject(p2_1.Name),
+    doc.getObject(p3_1.Name),
+    doc.getObject(p1_2.Name),
+    doc.getObject(p2_2.Name),
+    doc.getObject(p3_2.Name)
+]
+step_name = "all.step"
+export_path = os.path.join(script_dir, step_name)
+print("Exporting as", export_path)
+Part.export(objs, export_path) #r"C:\Users\Ocanath Robotman\Desktop\PLANETSTEP\p3Stack.step"
+
+print("Done!")
---
+++ gear_ratio_calculator.py
@@ -0,0 +1,113 @@
+import numpy as np
+
+
+"""
+	Set to 1 if you want the planet to have 1 tooth less than the matching pitch
+	depth of the ring, for tolerance. Set to 0 if you want the 'normal'/canonical 
+	pitch mating distance
+"""
+drop_tooth_ring1 = 0	# 1 = backlash/tolerance using drop tooth technique. 0 = no backlash but might not work due to tolerance of manufactured gear
+drop_tooth_ring2 = 0	
+
+"""
+set the desired number of ring and planet teeth, as well as pitch
+"""
+p1_teeth = 21
+r1_teeth = 54
+r2_teeth = 53
+sun_teeth = r1_teeth - drop_tooth_ring1*2 - 2*p1_teeth
+p2_teeth = r2_teeth - sun_teeth - p1_teeth - drop_tooth_ring2*2	#should be always p1 -1?
+pitch_mm = 0.5
+
+
+"""
+set the number of desired planets
+"""
+num_planets = 3
+
+r_r1 = r1_teeth * pitch_mm / 2
+r_r2 = r2_teeth * pitch_mm / 2
+r_s = sun_teeth * pitch_mm / 2
+r_p11 = p1_teeth * pitch_mm / 2
+r_p12 = p1_teeth * pitch_mm / 2
+r_p2 = p2_teeth * pitch_mm / 2
+
+I1 = (r_r1*r_p11)/(r_s*r_p12)
+I2 = (r_r1*r_p2)/(r_r2*r_p12)
+
+G = (1 - I2)/(1 + I1)
+
+
+carrier_planets_to_sun_distance = ((p1_teeth + drop_tooth_ring1) * pitch_mm / 2) + (sun_teeth * pitch_mm / 2)
+
+print( "sun teeth", sun_teeth)
+print("planet 1 teeth", p1_teeth)
+print ("ring 1 teeth", r1_teeth)
+print("planet 2 teeth", p2_teeth)
+print ("ring 2 teeth", r2_teeth)
+if(drop_tooth_ring1 != 0):
+	print("Drop tooth backlash present in stage 1")
+if(drop_tooth_ring2 != 0):
+	print("Drop tooth backlash present in stage 2")
+overallRatio = 1/G	#number of revolutions of the sun for one revolution of the output ring
+print ("gear ratio (sun:output ring)", overallRatio)
+print("r2 pitch diameter", r2_teeth*pitch_mm)
+print ("OD in mm", max(r1_teeth, r2_teeth)*pitch_mm)
+print("Carrier distance between sun and planet: " + str(carrier_planets_to_sun_distance) + "mm")
+sunToCarrier = 1/(sun_teeth/(r1_teeth+sun_teeth))
+print("sun to carrier ratio", sunToCarrier)
+print("If carrier is driven directly:", overallRatio/sunToCarrier)
+print("Max diameter of center bore for coax plug:", sun_teeth*pitch_mm)
+
+vdrive = 12
+print("WK3505 max speed in rpm at this gear ratio w."+str(vdrive)+"V driver", vdrive*72/(overallRatio/sunToCarrier), "required RPM is 12.60")
+
+"""
+	The following code determines whether an eccentric carrier is 
+	required, and computes the optimal carrier angles that will make it work
+"""
+def get_carrier_angles(num_planets, r1_teeth, sun_teeth):
+	angdiv = (r1_teeth + sun_teeth)/num_planets
+	if(np.floor(angdiv) == angdiv):
+		print("carrier spacing is " + str(360/num_planets))
+		ang = 360/num_planets
+		return [ang,ang,ang]
+	else:
+		print("Even spacing impossible. Computing optimal eccentric carrier angle...")
+		set = []	#this is the set of all angles which satisfy 
+		for n in range(1,10000000000):	#can change to while loop. this guarantees termination i guess? very kludged
+			anglep2p = (360/(r1_teeth + sun_teeth))*n
+			anglep2p = np.mod(anglep2p + 180, 360) - 180
+			if(np.floor(anglep2p) == anglep2p):
+				# print(anglep2p)
+				set.append(anglep2p)
+				if(len(set) > 0):
+					if(anglep2p < 0):
+						break
+		triplets = []
+		for i in range(0,len(set)):
+			for j in range(0,len(set)):
+				if(i != j):
+					val = 360 - (set[i] + set[j])
+					for k in range(0,len(set)):
+						if(set[k] == val):
+							# print("carrier option: (" + str(set[i]) + ", " + str(set[j]) + ", " + str(set[k]) + ")")
+							triplet_val = [set[i].copy(),set[j].copy(),set[k].copy()]
+							triplet_val.sort()
+							if triplet_val not in triplets:
+								triplets.append(triplet_val)
+		print(triplets[len(triplets)-1])
+		return triplets[len(triplets)-1];
+
+
+print("\n\nComputing required angles for carrier:")
+
+triplet = get_carrier_angles(num_planets, r1_teeth, sun_teeth)
+
+
+# ring2_secondcarrier_angle = np.round(triplet[2]/(360/r2_teeth))*360/r2_teeth
+# print("R2 center tooth to align angle is: "+str(ring2_secondcarrier_angle))
+# p2_angle_in_ring2_frame = ring2_secondcarrier_angle - (360/p2_teeth)/2
+# print("P2 angle (where plane intersects tooth center) is: "+str(p2_angle_in_ring2_frame)+" wrt center plane of ring2")
+
+
