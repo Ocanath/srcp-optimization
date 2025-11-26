c18f3ba7c1d3afd9bde198938dacea8165959cf7
Jesse Cornman <ocanath@gmail.com>
2025-11-25 19:31:53 -0800

    notes on factorization

--- misc-tools/factors.py
+++ misc-tools/factors.py
@@ -1,5 +1,11 @@
 import numpy as np
+"""
+If you want a module which does not have an infinitely repeating decimal, you must
+ensure the module equation m * (np1 - nr1)/(np2 - nr2) has a quotient which has prime
+factors of 2 and 5.
 
+This lists all the prime factors between 0 and 120
+"""
 nums = []
 for n in range(0,10):
     for m in range(0,10):
@@ -7,3 +13,4 @@ for n in range(0,10):
 nums.sort()
 nums = np.array(nums)
 print(nums[nums<=120])
+
